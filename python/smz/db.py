cca61c5e5257cfe06261166869fc9eba4116ea6d
MoriatyC <119889525@qq.com>
2017-03-14 20:03:49 +0800

    finish db module

--- python/smz/db.py
+++ python/smz/db.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 import MySQLdb as My
-conn = My.connect(db='Medical_Information', host='192.168.2.79', user='root', passwd='root', charset='utf8')
+conn = My.connect(db='Medical_Information', host='192.168.2.79', port=3306, user='root', passwd='root', charset='utf8')
 cur = conn.cursor()
-string = 'insert into patient_information values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-print string
 
-if cur.execute(string, ('才', '才', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 211)):
-    print '插入成功'
-else:
-    print '插入失败'
-cur.close()
-conn.commit()
-conn.close()
+
+def insert(string):
+    try:
+        cur.execute(string)
+        print '插入成功'
+    except Exception, e:
+        print e
+    finally:
+        cur.close()
+        conn.commit()
+        conn.close()
 
 
 def show():
-    # string = 'insert into patient_information values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    cur.execute("select * from patient_information")
-    for i in cur.fetchall():
-        print i
+    try:
+        cur.execute("select * from patient_information")
+        for i in cur.fetchall():
+            print i
+    except Exception, e:
+        print e
+    finally:
+        cur.close()
+        conn.commit()
+        conn.close()
+
+
 if __name__ == '__main__':
-    pass
+    show()
+
+
+
--- python/smz/extract.py
+++ python/smz/extract.py
@@ -4,11 +4,13 @@ import os
 import re
 import pywintypes
 import db
+keys = []
+values = []
 BASE_DIR = os.path.dirname(__file__)
 file_in = os.path.join(BASE_DIR, 'in/1.doc')
 file_out = os.path.join(BASE_DIR, 'out/1.txt')
-# 打开word进程
 try:
+    # 打开word进程
     word = wc.Dispatch('Word.Application')
     # 打开相应的word文件
     doc = word.Documents.Open(file_in)
@@ -20,116 +22,26 @@ try:
     pat = re.compile(r'(\s\S*?)：(\s\S*)')
     ret = pat.findall(text)
     for i in range(16):
-        print str(i+1) + "  " + ret[i][0].strip() + ":" + ret[i][1].strip()
+        keys.append(ret[i][0].strip())
+        values.append(ret[i][1].strip())
     progress_note = re.compile(r'入院诊断：([\s\S]*?)出院诊断：([\s\S]*?)病程记录([\s\S]*)')
+    keys.append('入院诊断')
+    keys.append('出院诊断')
+    keys.append('病程记录')
     ret_note = progress_note.search(text)
     if ret_note:
-        print ret_note.group(1).strip()
-        print '='*100
-        print ret_note.group(2).strip()
-        print '+' * 100
-        print ret_note.group(3).strip()
+        values.append(ret_note.group(1).strip())
+        values.append(ret_note.group(2).strip())
+        values.append(ret_note.group(3).strip())
     else:
         print 'ret_note search failed'
-    # if ret_name:
-    #     print ret_name.group(1).strip()
-    # else:
-    #     print 'ret_name search failed'
-    #
-    # if ret_sex:
-    #     print ret_sex.group(1).strip()
-    # else:
-    #     print 'ret_sex search failed'
-    #
-    # if ret_age:
-    #     print ret_age.group(1).strip()
-    # else:
-    #     print 'ret_age search failed'
-    #
-    # if ret_nation:
-    #     print ret_nation.group(1).strip()
-    # else:
-    #     print 'ret_nation search failed'
-    #
-    # if ret_career:
-    #     print ret_career.group(1).strip()
-    # else:
-    #     print 'ret_career search failed'
-    #
-    # if ret_address:
-    #     print ret_address.group(1).strip()
-    # else:
-    #     print 'ret_address search failed'
-    #
-    # if ret_hometown:
-    #     print ret_hometown.group(1).strip()
-    # else:
-    #     print 'ret_hometown search failed'
-    #
-    # if ret_workplace:
-    #     print ret_workplace.group(1).strip()
-    # else:
-    #     print 'ret_workplace search failed'
-    #
-    # if ret_marital_status:
-    #     print ret_marital_status.group(1).strip()
-    # else:
-    #     print 'ret_marital_status search failed'
-    #
-    # if ret_admission_time:
-    #     print ret_admission_time.group(1).strip()
-    # else:
-    #     print 'ret_admission_time search failed'
-    #
-    # if ret_chief_complaint:
-    #     print ret_chief_complaint.group(1).strip()
-    # else:
-    #     print 'ret_chief_complaint search failed'
-    #
-    # if ret_history_of_present_illness:
-    #     print ret_history_of_present_illness.group(1).strip()
-    # else:
-    #     print 'ret_history_of_present_illness search failed'
-    #
-    # if ret_past_history:
-    #     print ret_past_history.group(1).strip()
-    # else:
-    #     print 'ret_past_history search failed'
-    #
-    # if ret_personal_history:
-    #     print ret_personal_history.group(1).strip()
-    # else:
-    #     print 'ret_personal_history search failed'
-    #
-    # if ret_menstrual_history:
-    #     print ret_menstrual_history.group(1).strip()
-    # else:
-    #     print 'ret_menstrual_history search failed'
-    #
-    # if ret_family_history:
-    #     print ret_family_history.group(1).strip()
-    # else:
-    #     print 'ret_family_history search failed'
-    #
-    # if ret_physical_examination:
-    #     print ret_physical_examination.group(1).strip()
-    # else:
-    #     print 'ret_physical_examination search failed'
-    #
-    # if ret_admitting_diagnosis:
-    #     print ret_admitting_diagnosis.group(1).strip()
-    # else:
-    #     print 'ret_admitting_diagnosis search failed'
-    #
-    # if ret_discharge_diagnosis:
-    #     print ret_discharge_diagnosis.group(1).strip()
-    # else:
-    #     print 'ret_discharge_diagnosis search failed'
-
-    # if progress_note:
-    #     print ret_progress_note.group()
-    # else:
-    #     print 'progress_note search failed'
+    keys.extend(values)
+    template = tuple(keys)
+    string = "insert into patient_information " \
+             "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)values" \
+             "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'," \
+             "'%s','%s','%s','%s','%s')" % template
+    db.insert(string)
 except pywintypes.com_error:
     pass
 finally:
