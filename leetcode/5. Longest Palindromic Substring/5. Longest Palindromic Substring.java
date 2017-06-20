b0c635376263d959036ff89b218443eb2c8d2fa3
MoriatyC <119889525@qq.com>
2017-06-20 08:24:11 +0800

    5:

--- leetcode/5. Longest Palindromic Substring/5. Longest Palindromic Substring.java
+++ leetcode/5. Longest Palindromic Substring/5. Longest Palindromic Substring.java
@@ -4,7 +4,7 @@ public class Solution {
         char[] trans = new char[2 * str.length + 5];
         int length = str.length * 2 + 1;
         int[] len = new int[length + 10];
-        int max = 0;
+        int max = 0;//第一个最长最长回文子串最右边的字母，偶数时代表右边那一个
         int ret = 0;
         int po = 0;
         int index = -1;
@@ -18,15 +18,15 @@ public class Solution {
         trans[length + 2] = 0;
         for (int i = 1; i <= length; i++) {
             if (i < max) {
-                len[i] = Math.min(max - i+1, len[2 * po - i]);
+                len[i] = Math.min(max - i + 1, len[2 * po - i]);
             } else {
                 len[i] = 1;
             }
             while (trans[i + len[i]] == trans[i - len[i]]) {
                 len[i]++;
             }
-            if (max < i + len[i]-1) {
-                max = i + len[i]-1;
+            if (max < i + len[i] - 1) {
+                max = i + len[i] - 1;
                 po = i;
             }
             if (ret < len[i]) {
