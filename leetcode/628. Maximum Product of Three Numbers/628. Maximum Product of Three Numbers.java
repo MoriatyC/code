1726b47bf6cc3a037c842241d701f2dae22ff013
MoriatyC <119889525@qq.com>
2017-06-27 15:31:41 +0800

    628

---
+++ leetcode/628. Maximum Product of Three Numbers/628. Maximum Product of Three Numbers.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        return Math.max(nums[len - 1] * nums[len - 2] * nums[len - 3], nums[len - 1] * nums[1] * nums[0]) ;
+    }
+}
