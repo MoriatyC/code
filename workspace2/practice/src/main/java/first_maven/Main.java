76166d1f0298e90c5ff0c50ece4fe7751b12491c
MoriatyC <119889525@qq.com>
2018-01-19 16:07:23 +0800

    add

--- workspace2/practice/src/main/java/first_maven/Main.java
+++ workspace2/practice/src/main/java/first_maven/Main.java
@@ -1,57 +1,72 @@
-package first_maven;
+package com.dlut.cmh.practice;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
-
+import java.util.Random;
 
 public class Main {
-    public int movingCount(int threshold, int rows, int cols)
-    {
-        if (threshold < 0 || rows <= 0 || cols <= 0) {
-            return 0;
+    public static int partion(int[] arr, int left, int right) {
+        if (left >= right) {
+            return left;
         }
-        boolean[][] visited = new boolean[rows][cols];
-        int row = 0;
-        int col = 0;
-        int[] ret = {0};
-        backtracking(col, row, cols, rows, threshold, visited, ret);
-        return ret[0];
+        Random random = new Random();
+        int pivot = random.nextInt(right - left + 1) + left;
+        swap(arr, pivot, right);
+        int small = left - 1;
+        for (int i = left; i < right; i++) {
+            if (arr[i] < arr[right]) {
+                small++;
+                if (i != small) {
+                    swap(arr, i, small);
+                }
+            }
+        }
+        swap(arr, ++small, right);
+        return small;
     }
-    public void backtracking(int col, int row, int cols, int rows, int threshold, boolean[][] visited, int[] ret) {
-        if (col < 0 || row < 0 || col >= cols || row >= rows || visited[row][col]) {
+    public static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+    public static void solution(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || k <= 0) {
             return;
         }
-        if (canPass(col, row, threshold)) {
-            visited[row][col] = true;
-            ret[0]++;
-            backtracking(col, row + 1, cols, rows, threshold, visited, ret);
-            backtracking(col, row - 1, cols, rows, threshold, visited, ret);
-            backtracking(col + 1, row, cols, rows, threshold, visited, ret);
-            backtracking(col - 1, row, cols, rows, threshold, visited, ret);
-        }
-    }
-    public boolean canPass(int col, int row, int threshold) {
-        int sum = 0;
-        while (col > 0) {
-            sum += col % 10;
-            if (sum > threshold) {
-                return false;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
             }
-            col /= 10;
-        }
-        while (row > 0) {
-            sum += row % 10;
-            if (sum > threshold) {
-                return false;
+        });
+        pq.offer(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(arr[i]);
+            } else {
+                int top = pq.peek();
+                if (top > arr[i]) {
+                    pq.poll();
+                    pq.offer(arr[i]);
+                }
             }
-            row /= 10;
         }
-        return true;
+        for (int i : pq) {
+            System.out.println(i);
+        }
     }
     public static void main(String[] args) {
-        Main m = new Main();
-        System.out.println(m.movingCount(5, 10, 10));
-        
+        List<String> list = new ArrayList<>();
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String s1 = o1 + o2;
+                String s2 = o2 + o1;
+                return Integer.valueOf(s2) - Integer.valueOf(s1);
+            }
+        });
     }
 }
