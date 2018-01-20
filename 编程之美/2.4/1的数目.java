7ad06df125ef363896291730f37d1d4208b06eb8
MoriatyC <119889525@qq.com>
2018-01-20 17:59:16 +0800

    add

---
+++ 编程之美/2.4/1的数目.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int x = 1;
+        int count = 0;
+        int rest = n;
+        do {
+            int cur = rest % 10;
+            rest /= 10;
+            if (cur == 0) {
+                count += rest * x;
+            } else if (cur > 1) {
+                count += (rest + 1) * x; 
+            } else {
+                count += rest * x + n % x + 1;
+            }
+            x *= 10;
+        } while (rest != 0);
+        return count;
+    }
+}
