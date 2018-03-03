392f56575ffc2ad9dffa4ab1c8dbdeb301619d06
MoriatyC <119889525@qq.com>
2018-03-03 15:47:29 +0800

    [add] r2

---
+++ 剑指offer R2/15.链表中导数第K个节点/solution.java
@@ -0,0 +1,28 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if (k < 0) {
+            return null;
+        }
+        ListNode fast = head;
+        for (int i = 1; i <= k; i++) {
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            head = head.next;
+        }
+        return head;
+    }
+}
