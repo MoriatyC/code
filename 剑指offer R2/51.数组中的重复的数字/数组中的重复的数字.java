f12039c94c186f8253764fe70494a4575372dc83
MoriatyC <119889525@qq.com>
2018-03-24 16:10:49 +0800

    [add]不用加减乘除做加法

---
+++ 剑指offer R2/51.数组中的重复的数字/数组中的重复的数字.java
@@ -0,0 +1,24 @@
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int nums[],int length,int [] duplication) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int[] arr = new int[length];
+        for (int i: nums) {
+            if (arr[i] != 0) {
+                duplication[0] = i;
+                return true;
+            }
+            arr[i]++;
+        }
+        return false;
+    }
+}
