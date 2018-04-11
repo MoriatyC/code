a089eaf0a916f80db56a307683c871715e7425d9
MoriatyC <119889525@qq.com>
2018-04-11 19:06:01 +0800

    [add]162

---
+++ leetcodeR2/162. Find Peak Element/162. Find Peak Element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (nums[mid] < nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+            
+        }
+        return nums[lo] > nums[hi] ? lo : hi;
+    }
+}
