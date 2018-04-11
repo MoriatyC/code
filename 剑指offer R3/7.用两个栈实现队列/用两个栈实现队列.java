113357a672a7d15a2838444dd7d0026e9e094b64
MoriatyC <119889525@qq.com>
2018-04-11 14:30:43 +0800

    [add]7

---
+++ 剑指offer R3/7.用两个栈实现队列/用两个栈实现队列.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        stack1.push(node);
+    }
+    
+    public int pop() {
+        if (stack1.empty() && stack2.empty()) {
+            System.out.println("队列为空请检查");
+            return 0;
+        }
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
