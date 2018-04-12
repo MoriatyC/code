1905cfb71dc0927bdfe1f51db4eb66a8099830bc
MoriatyC <119889525@qq.com>
2018-04-12 09:17:51 +0800

    [add]16

---
+++ 剑指offer R3/16.反转链表/16.反转链表.java
@@ -0,0 +1,35 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
+/*if (head == null) {
+            return head;
+        }
+        ListNode pre = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        return pre;
+
+*/
