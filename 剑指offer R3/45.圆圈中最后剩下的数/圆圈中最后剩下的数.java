180ddd8d87c1c767b7c431a62df285d6d7b71b1c
MoriatyC <119889525@qq.com>
2018-04-24 09:24:37 +0800

    [add]45

---
+++ 剑指offer R3/45.圆圈中最后剩下的数/圆圈中最后剩下的数.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Solution {
+    public int LastRemaining_Solution(int n, int m) {
+        if (n <= 0 || m <= 0) {
+            return -1;
+        }
+        int ret = 0;
+       for (int i = 2; i <= n; i++) {
+           ret = (ret + m) % i;
+       }
+        return ret;
+    }
+}
