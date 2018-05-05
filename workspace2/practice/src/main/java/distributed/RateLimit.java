0b3b57ebb2734efe08da12e273bb8ca611d7b920
MoriatyC <119889525@qq.com>
2018-05-05 17:32:20 +0800

    rate limit demo

--- workspace2/practice/src/main/java/distributed/RateLimit.java
+++ workspace2/practice/src/main/java/distributed/RateLimit.java
@@ -1,5 +1,7 @@
 package distributed;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.util.concurrent.RateLimiter;
@@ -9,15 +11,51 @@ import com.google.common.util.concurrent.RateLimiter;
  * @version ：2018年5月5日 下午4:03:35
  * @Description:
  */
+
+
 public class RateLimit {
-    public static void main(String[] args) {
-        RateLimiter rateLimiter = RateLimiter.create(1.0);
-        Long start = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) {
-            rateLimiter.acquire(1);
-            System.out.println(i);
-            System.out.println("call execute.." + i);  
+    private int permitsPerSecond = 4;
+    private int threadNum = 3;
+    private void call() {
+        ExecutorService executor = Executors.newFixedThreadPool(threadNum);
+        final RateLimiter rateLimiter = RateLimiter.create(permitsPerSecond);
+        for (int i = 0; i < threadNum; i++) {
+            executor.execute(new Mytask(rateLimiter, (char)('A' + i)));
         }
-        System.out.println(System.currentTimeMillis() - start);
+        executor.shutdown();
+    }
+    public static void main(String[] args) {
+        new RateLimit().call();
+    }
+    
+}
+
+
+class Mytask implements Runnable {
+    private RateLimiter rateLimiter;
+    private boolean running = true;
+    private char c;
+    public Mytask(RateLimiter rateLimiter, char c) {
+        this.rateLimiter = rateLimiter;
+        this.c = c;
+    }
+    @Override
+    public void run() {
+        while (running) {
+            rateLimiter.acquire(4);
+            getData();
+        }
+    }
+    private void getData()  {
+        System.out.println(Thread.currentThread().getName()+ "   " + c + "is running");
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
